8eab903c13523a803d1bb07d8e36c69a8be48278
shoushimaya <shoushimaya@gmail.com>
2022-08-02 02:54:00 -0700

    created login and register view

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import *
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'bf44ed945ec08fb0b84b1c7b9aed0325'
+
+
+@app.route("/")
+@app.route("/welcome")
+def welcome():
+    return render_template('welcome.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    # if form.validate_on_submit():
+    #     flash(f'Account created for {form.username.data}!', 'success')
+    #     return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    # if form.validate_on_submit():
+    #     if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+    #         flash('You have been logged in!', 'success')
+    #         return redirect(url_for('home'))
+    #     else:
+    #         flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
