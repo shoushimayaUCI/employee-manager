848abb1238e169a559fbd2d738dde9b9be3cc7c2
shoushimaya <shoushimaya@gmail.com>
2022-08-06 17:54:10 -0700

    minor bug fix

--- employee_manager/forms.py
+++ employee_manager/forms.py
@@ -1,17 +1,17 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from flask_wtf.file import FileField, FileAllowed
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flask_login import current_user
+
 from employee_manager.models import User
 
 # add team functionality later
 class RegistrationForm(FlaskForm):
-	username = StringField('Username', 
-		validators=[DataRequired(), Length(min=2, max=20)])
-	email = StringField('Email',
-		validators=[DataRequired(), Email()])
+	username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+	email = StringField('Email', validators=[DataRequired(), Email()])
 	password = PasswordField('Password', validators=[DataRequired()])
-	title = StringField('Job title',
-		validators=[DataRequired(), Length(min=2, max=50)])
+	title = StringField('Job title', validators=[DataRequired(), Length(min=2, max=50)])
 	confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
 	submit = SubmitField('Sign up')
 
@@ -35,3 +35,27 @@ class LoginForm(FlaskForm):
 	def validate_email(self, email):
 		if not User.query.filter_by(email=email.data).first():
 			raise ValidationError('email does not exist')
+
+
+class UpdateAccountForm(FlaskForm):
+	username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+	email = StringField('Email', validators=[DataRequired(), Email()])
+	title = StringField('Job title', validators=[DataRequired(), Length(min=2, max=50)])
+	picture = FileField('Update Profile Picture', 
+		validators=[FileAllowed(['jpg', 'png'])])
+	submit = SubmitField('Update')
+
+	def validate_username(self, username):
+		if (username.data != current_user.username and 
+				User.query.filter_by(username=username.data).first()):
+			raise ValidationError('username taken')
+	
+	def validate_email(self, email):
+		if (email.data != current_user.email and 
+				User.query.filter_by(email=email.data).first()):
+			raise ValidationError('email taken')
+	
+
+
+
+
--- employee_manager/routes.py
+++ employee_manager/routes.py
@@ -1,8 +1,11 @@
 from flask import render_template, url_for, flash, redirect, request
 from flask_login import login_user, logout_user, current_user, login_required
+import secrets
+from PIL import Image
+import os 
 
 from employee_manager import app, db, bcrypt
-from employee_manager.forms import RegistrationForm, LoginForm
+from employee_manager.forms import *
 from employee_manager.models import User
 
 @app.route("/")
@@ -45,7 +48,36 @@ def logout():
     return redirect(url_for('welcome'))
 
 
-@app.route("/account")
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    file_name = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile-pics', file_name)
+    
+    # rescaling the size of images we save
+    i= Image.open(form_picture)
+    i.thumbnail((125, 125))
+    i.save(picture_path)
+    return file_name
+
+
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html', title='Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.title = form.title.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('your account has been updated', category='success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.title.data = current_user.title
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile-pics/'+current_user.image_file)
+    return render_template('account.html', title='Account', form=form, image_file=image_file)
