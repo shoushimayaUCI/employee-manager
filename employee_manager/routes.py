603a48c0549b8c27b0b1049912a158631331d7b1
shoushimaya <shoushimaya@gmail.com>
2022-08-24 14:43:17 -0700

    finished changing everything to use REST API

--- employee_manager/routes.py
+++ employee_manager/routes.py
@@ -43,24 +43,29 @@ class register(Resource):
         return make_response(render_template('register.html', title='Register', form=form))
 
 
-@app.route("/login", methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if bcrypt.check_password_hash(user.password, form.password.data):
-            login_user(user, remember=form.remember.data)
-            next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('home'))
-        else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
-    return render_template('login.html', title='Login', form=form)
+class login(Resource):
+    def get(self):
+        return make_response(render_template('login.html', title='Login', form=LoginForm()))
 
-@app.route("/logout")
-def logout():
-    logout_user()
-    flash('Logout Successful')
-    return redirect(url_for('welcome'))
+    def post(self):
+        form = LoginForm()
+        if form.validate_on_submit():
+            user = User.query.filter_by(email=form.email.data).first()
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember.data)
+                next_page = request.args.get('next')
+                return make_response(redirect(next_page) if next_page else redirect(url_for('home')))
+            else:
+                flash('Login Unsuccessful. Please check username and password', 'danger')
+        return make_response(render_template('login.html', title='Login', form=form))
+
+
+class logout(Resource):
+    @login_required
+    def get(self):
+        logout_user()
+        flash('Logout Successful')
+        return make_response(redirect(url_for('welcome')))
 
 
 def save_picture(form_picture):
@@ -76,79 +81,105 @@ def save_picture(form_picture):
     return file_name
 
 
-@app.route("/account", methods=['GET', 'POST'])
-@login_required
-def account():
-    form = UpdateAccountForm()
-    if form.validate_on_submit():
-        if form.picture.data:
-            picture_file = save_picture(form.picture.data)
-            current_user.image_file = picture_file
-        current_user.username = form.username.data
-        current_user.title = form.title.data
-        current_user.email = form.email.data
-        current_user.team_id = form.team_id.data
-        db.session.commit()
-        flash('your account has been updated', category='success')
-        return redirect(url_for('account'))
-    elif request.method == 'GET':
-        form.username.data = current_user.username
-        form.title.data = current_user.title
-        form.email.data = current_user.email
-        form.team_id.data = current_user.team_id
-    image_file = url_for('static', filename='profile-pics/'+current_user.image_file)
-    return render_template('account.html', title='Account', form=form, image_file=image_file)
+class account(Resource):
+    @login_required
+    def get(self):
+        form = UpdateAccountForm()
+        image_file = url_for('static', filename='profile-pics/'+current_user.image_file)
+        return make_response(render_template('account.html', title='Account', form=form, image_file=image_file))
+
+    @login_required
+    def post(self):
+        form = UpdateAccountForm()
+        if form.validate_on_submit():
+            if form.picture.data:
+                picture_file = save_picture(form.picture.data)
+                current_user.image_file = picture_file
+            current_user.username = form.username.data
+            current_user.title = form.title.data
+            current_user.email = form.email.data
+            current_user.team_id = form.team_id.data
+            db.session.commit()
+            flash('your account has been updated', category='success')
+            return make_response(redirect(url_for('account')))
+        elif request.method == 'GET':
+            form.username.data = current_user.username
+            form.title.data = current_user.title
+            form.email.data = current_user.email
+            form.team_id.data = current_user.team_id
+        image_file = url_for('static', filename='profile-pics/'+current_user.image_file)
+        return make_response(render_template('account.html', title='Account', form=form, image_file=image_file))
 
 
-@app.route("/new_task", methods=['GET', 'POST'])
-@login_required
-def new_task():
-    form = NewTaskForm()
-    if form.validate_on_submit():
-        if not form.person.data:
-            form.person.data = current_user.email
-        db.session.add(Task(title=form.title.data, description=form.description.data, 
-            person=form.person.data))
-        db.session.commit()
-        flash('The task has been created!', 'success')
-        return redirect(url_for('home'))
-    return render_template('new_task.html', form=form)
+class new_task(Resource):
+    @login_required
+    def get(self):
+        return make_response(render_template('new_task.html', form=NewTaskForm()))
+
+    @login_required
+    def post(self):
+        form = NewTaskForm()
+        if form.validate_on_submit():
+            if not form.person.data:
+                form.person.data = current_user.email
+            db.session.add(Task(title=form.title.data, description=form.description.data, 
+                person=form.person.data))
+            db.session.commit()
+            flash('The task has been created!', 'success')
+            return make_response(redirect(url_for('home')))
+        return make_response(render_template('new_task.html', form=form))
 
 
-@app.route("/announcements")
-@login_required
-def announcements():
-    subquery = db.session.query(Announcement).filter(Announcement.team==current_user.team_id)
-    return render_template('announcements.html', announcements=subquery.all())
+class announcements(Resource):
+    @login_required
+    def get(self):
+        subquery = db.session.query(Announcement).filter(Announcement.team==current_user.team_id)
+        return make_response(render_template('announcements.html', announcements=subquery.all()))
 
 
-@app.route("/new_announcement", methods=['GET', 'POST'])
-@login_required
-def new_announcement():
-    form = AnnouncementForm()
-    if form.validate_on_submit():
-        if not form.team.data:
-            form.team.data = current_user.team_id
-        db.session.add(Announcement(title=form.title.data, author=current_user.email,
-            description=form.description.data, team=form.team.data))
-        db.session.commit()
-        flash('You successfully made an announcement!', 'success')
-        return redirect(url_for('announcements'))
-    return render_template('new_announcement.html', form=form)
+class new_announcement(Resource):
+    @login_required
+    def get(self):
+        return make_response(render_template('new_announcement.html', form=AnnouncementForm()))
+
+    @login_required
+    def post(self):
+        form = AnnouncementForm()
+        if form.validate_on_submit():
+            if not form.team.data:
+                form.team.data = current_user.team_id
+            db.session.add(Announcement(title=form.title.data, author=current_user.email,
+                description=form.description.data, team=form.team.data))
+            db.session.commit()
+            flash('You successfully made an announcement!', 'success')
+            return make_response(redirect(url_for('announcements')))
+        return make_response(render_template('new_announcement.html', form=form))
 
 
-@app.route("/new_team", methods=['GET', 'POST'])
-@login_required
-def new_team():
-    form = TeamForm()
-    if form.validate_on_submit():
-        db.session.add(Team(team_id=form.team_id.data, team_name=form.team_name.data))
-        db.session.commit()
-        flash('You successfully made a team!', 'success')
-        return redirect(url_for('home'))
-    return render_template('new_team.html', form=form)
+class new_team(Resource):
+    @login_required
+    def get(self):
+        return make_response(render_template('new_team.html', form=TeamForm()))
+
+    @login_required
+    def post(self):
+        form = TeamForm()
+        if form.validate_on_submit():
+            db.session.add(Team(team_id=form.team_id.data, team_name=form.team_name.data))
+            db.session.commit()
+            flash('You successfully made a team!', 'success')
+            return make_response(redirect(url_for('home')))
+        return make_response(render_template('new_team.html', form=form))
 
 
 api.add_resource(welcome, "/")
 api.add_resource(register, '/register')
+api.add_resource(about, '/about')
 api.add_resource(home, '/home')
+api.add_resource(login, '/login')
+api.add_resource(logout, '/logout')
+api.add_resource(account, '/account')
+api.add_resource(new_task, '/new_task')
+api.add_resource(announcements, '/announcements')
+api.add_resource(new_announcement, '/new_announcement')
+api.add_resource(new_team, '/new_team')
